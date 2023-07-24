51d4948560d8bbe3372e9f8dfa6f0f646950d4ff
Mahdizadeh <Amirali.mahdizadeh81@gmail.com>
2023-07-24 19:16:37 +0330

    completely checked and have invisibility bug

--- app/src/main/java/com/efficientcalculater/MainActivity.kt
+++ app/src/main/java/com/efficientcalculater/MainActivity.kt
@@ -2,14 +2,56 @@ package com.efficientcalculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    private val input = mutableListOf<String>()
+    private var resultTextBox: TextView? = null
+    private var infixExpression: Expression? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultTextBox = findViewById(R.id.resultTextBox)
     }
-    fun onClick(button:View) {
+
+    fun onClick(button: View) {
+        when (val buttonText = (button as AppCompatButton).text.toString()) {
+            "=" -> {
+                infixExpression = Expression(input)
+                resultTextBox?.text = infixExpression!!.evaluateExpression().toString()
+            }
+
+            "CL" -> {
+                input.clear()
+                resultTextBox?.text = ""
+            }
+
+            "C" -> {
+                resultTextBox?.text = "${resultTextBox?.text}".dropLast(input.last().length)
+                input.remove(input.last())
+            }
+
+            else -> {
+                if (Character.isDigit(buttonText[0])) {
+                    if (input.isNotEmpty() && Character.isDigit(input.last()[0])) {
+                        input[input.lastIndex] = input.last() + buttonText
+                        resultTextBox?.text = "${resultTextBox?.text}${button.text}"
+                    } else {
+                        input.add(buttonText)
+                        resultTextBox?.text = "${resultTextBox?.text} ${button.text}"
+                    }
+
+                } else {
+                    input.add(buttonText)
+                    resultTextBox?.text = "${resultTextBox?.text} ${button.text}"
+                }
+
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/efficientcalculater/expretion.kt
+++ app/src/main/java/com/efficientcalculater/expretion.kt
@@ -1,9 +1,11 @@
 package com.efficientcalculater
 
+import android.util.Log
 import java.util.Stack
 
-class Expression(var infixExpression: MutableList<String>) {
-    private fun infixToPostfix(): String {
+class Expression(private var infixExpression: MutableList<String>) {
+    private var postfix: String = ""
+    private fun infixToPostfix() {
         var result = ""
         val stack = Stack<String>()
         for (element in infixExpression) {
@@ -12,8 +14,8 @@ class Expression(var infixExpression: MutableList<String>) {
             } else if (element == "(") {
                 stack.push(element)
             } else if (element == ")") {
-                while (stack.peek() != ")" && stack.isNotEmpty()) {
-                    result = "${stack.pop()} "
+                while (stack.peek() != "(" && stack.isNotEmpty()) {
+                    result += "${stack.pop()} "
                 }
                 if (stack.isNotEmpty())
                     stack.pop()
@@ -21,31 +23,34 @@ class Expression(var infixExpression: MutableList<String>) {
                 while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(element)) {
                     result += "${stack.pop()} "
                 }
+                stack.push(element)
             }
         }
         while (stack.isNotEmpty()) {
             result += "${stack.pop()} "
         }
-        return result
+        Log.d("infixToPostfix", "result is :$result")
+        postfix = result
     }
 
     private fun precedence(operator: String): Int {
         return when (operator) {
-            "*", "/" -> 2
+            "×", "÷" -> 2
             "-", "+" -> 1
             else -> -1
         }
     }
 
-    fun evaluateExpression(postfix: String): Number {
+    fun evaluateExpression(): Number {
+        infixToPostfix()
         var i = 0
         val stack = Stack<Double>()
-        var number = ""
         while (i < postfix.length) {
             if (postfix[i] == ' ') {
                 i++
                 continue
             } else if (Character.isDigit(postfix[i])) {
+                var number = ""
                 while (Character.isDigit(postfix[i]) || postfix[i] == '.') {
                     number += postfix[i]
                     i++
@@ -55,15 +60,16 @@ class Expression(var infixExpression: MutableList<String>) {
                 val x = stack.pop()
                 val y = stack.pop()
                 when (postfix[i]) {
-                    '*' -> stack.push(x * y)
-                    '/' -> stack.push(x / y)
-                    '-' -> stack.push(x - y)
+                    '×' -> stack.push(x * y)
+                    '÷' -> stack.push(y / x)
+                    '-' -> stack.push(y - x)
                     '+' -> stack.push(x + y)
                 }
             }
             i++
         }
-        return if (stack.peek() / stack.pop().toInt() == 1.0) stack.peek().toInt() else stack.peek()
+        return if ((stack.peek() / stack.peek().toInt()) == 1.0) stack.peek()
+            .toInt() else stack.peek()
     }
 
 }
